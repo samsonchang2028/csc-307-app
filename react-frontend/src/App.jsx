f70b57e41af6793b43fb2c3c99d46321aa8f547b
samsonchang2028 <schan119@calpoly.edu>
2026-04-03 09:04:30 -0700

    added npm to script and also changed the HMR to work + message under Get Started

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Hi from 307 Lab
           </p>
         </div>
         <button

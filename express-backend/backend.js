40a9b458146b547c00c8b8f42eb4c2905676e3f0
Samson Chang <schan119@calpoly.edu>
2026-04-14 10:28:47 -0700

    ie2 stage 2: adding users and seeing it on /users

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -8,10 +8,47 @@ const port = 8000;
 
 app.use(express.json());
 
-
+// get method for hello world
 app.get('/', (req, res )=>{
     res.send("Hello world!");
 });
+
+
+//get method to send users over 
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+
 app.listen(port, ()=> {
     console.log(`example app listening at http://localhost:${port}`)
 });
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};

34a33a05645546e1cade6376b5ac137f946c48d3
Samson Chang <schan119@calpoly.edu>
2026-04-14 10:41:20 -0700

    IE2: 5 searching using ID

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -15,23 +15,36 @@ app.get('/', (req, res )=>{
 
 
 //get method to send users over 
-// app.get("/users", (req, res) => {
-//   res.send(users);
-// });
+app.get("/users", (req, res) => {
+  res.send(users);
+});
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+// app.get("/users", (req, res) => {
+//   const name = req.query.name;
+//   if (name != undefined) {
+//     let result = findUserByName(name);
+//     result = { users_list: result };
+//     res.send(result);
+//   } else {
+//     res.send(users);
+//   }
+// });
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
   } else {
-    res.send(users);
+    res.send(result);
   }
 });
+
 
 app.listen(port, ()=> {
     console.log(`example app listening at http://localhost:${port}`)

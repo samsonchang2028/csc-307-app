df7cffb1d3af50dc95e6c8f7cd1c1759d2200511
samsonchang2028 <schan119@calpoly.edu>
2026-04-20 10:56:40 -0700

    ie3: added cors, added new functions to post on a submit from the frontend

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cors from "cors";
 
 
 const app = express();
@@ -7,7 +7,7 @@ const port = 8000;
 
 
 app.use(express.json());
-
+app.use(cors());
 
 app.get('/', (req, res )=>{
     res.send("Hello world!");
--- react-frontend/src/Table.jsx
+++ react-frontend/src/Table.jsx
@@ -1,47 +1,40 @@
 // src/Table.jsx
 function TableHeader() {
-    return (
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Job</th>
-            </tr>
-        </thead>
-    );
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
 }
 
 function TableBody(props) {
-    const rows = props.characterData.map((row, index) => {
-        return (
-            <tr key={index}>
-                <td>{row.name}</td>
-                <td>{row.job}</td>
-                <td>
-                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
-                </td>
-            </ tr>
-        );
-    }
-    );
+  const rows = props.characterData.map((row, index) => {
     return (
-        <tbody>
-            {rows}
-        </tbody>
-
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
     );
+  });
+  return <tbody>{rows}</tbody>;
 }
 
 function Table(props) {
-    return (
-        <table>
-            <TableHeader />
-            <TableBody
-                characterData={props.characterData}
-                removeCharacter={props.removeCharacter}
-            />
-
-        </table>
-    );
+  return (
+    <table>
+      <TableHeader />
+      <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
+    </table>
+  );
 }
 
 export default Table;
